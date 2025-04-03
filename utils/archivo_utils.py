8d789b66708ed3a97af843d4926ce415fec9a4eb
moafar <rafael.mizar@gmail.com>
2025-04-03 09:56:43 +0200

    Finalizado procesamiento DAM

--- utils/archivo_utils.py
+++ utils/archivo_utils.py
@@ -4,6 +4,7 @@ import logging
 from utils.texto_utils import extraer_texto_docx, extraer_texto_rtf, extraer_texto_doc, normalizar_texto, extraer_subcadenas, determinar_tipos_examenes
 from utils.procesar_psg import procesar_psg_doc, procesar_psg_rtf
 from utils.procesar_cpap import procesar_cpap_doc, procesar_cpap_rtf, procesar_cpap_docx
+from utils.procesar_dam import procesar_dam_doc, procesar_dam_rtf
 import csv
 
 def procesar_archivo(archivo: Path) -> None:
@@ -29,10 +30,12 @@ def procesar_archivo(archivo: Path) -> None:
         logging.error(f"Error inesperado al leer {archivo}: {e}")
         return None
 
+    #print(texto)  # Para verificar el texto extraído
     texto_normalizado = normalizar_texto(texto)  # Normalizar el texto extraído
     
+    #print(texto_normalizado)  # Para verificar el texto normalizado
     tipos_examenes = determinar_tipos_examenes(texto_normalizado)  # <-- Llamada a la función para determinar el tipo de examen ***
- 
+
     if not tipos_examenes:
         logging.warning(f"No se encontraron tipos de examen en el archivo {archivo}.")
         return
@@ -56,7 +59,8 @@ def procesar_archivo(archivo: Path) -> None:
             texto_relevante = extraer_subcadenas(texto_normalizado, inicio, fin) # <-- Llamada a la función para extraer subcadenas ***
             if texto_relevante:
                 logging.info(f"Subcadena encontrada para {tipo}: {texto_relevante}")
-                
+                #print(texto_relevante)  # Para verificar el texto relevante extraído
+                '''
                 if tipo == "BASAL":
                     if extension == ".rtf":
                         resultados_psg = procesar_psg_rtf(texto_relevante)
@@ -67,7 +71,7 @@ def procesar_archivo(archivo: Path) -> None:
                     else:
                         logging.warning(f"Extensión no reconocida para archivo: {archivo}")
                         continue
-                        
+                    
                     es_nuevo = not os.path.isfile(ruta) # Escribir encabezado si el archivo no existe
                     with open(ruta, mode='a', newline='', encoding='utf-8') as f:
                         writer = csv.DictWriter(f, fieldnames=resultados_psg.keys()) 
@@ -75,12 +79,10 @@ def procesar_archivo(archivo: Path) -> None:
                             writer.writeheader()
                         writer.writerow(resultados_psg)
                     logging.info(f"** FIN ** Procesamiento Basal terminado para {archivo}")
-                
-
-                '''elif tipo == "CPAP":''' # Cambiar del if al elif para enlazar con el procesamiento de PSG
-                if tipo == "CPAP":
+                    
+                elif tipo == "CPAP":
                     logging.info(f"** INICIO ** Procesando archivo CPAP válido: {archivo}")
-
+                    
                     if extension == ".rtf":
                         resultados_cpap = procesar_cpap_rtf(texto_relevante)
                         ruta = "resultados_cpap_rtf.csv"
@@ -101,10 +103,32 @@ def procesar_archivo(archivo: Path) -> None:
                             writer.writeheader()
                         writer.writerow(resultados_cpap)
                     logging.info(f"** FIN ** Procesamiento CPAP terminado para {archivo}")
+                '''
+                    
+                
+                if tipo == "DAM": # Cambiar a elif cuando esté terminado DAM para encadenar con PSG y CPAP
+                    logging.info(f"** INICIO ** Procesando archivo DAM válido: {archivo}")
+                    if extension == ".rtf":
+                        resultados_dam = procesar_dam_rtf(texto_relevante)
+                        print("\n",resultados_dam)
+                        ruta = "resultados_dam_rtf.csv"
+                    elif extension == ".doc":   
+                        resultados_dam = procesar_dam_doc(texto_relevante)
+                        print("\n",resultados_dam)
+                        ruta = "resultados_dam_doc.csv"
+                    else:
+                        logging.warning(f"Extensión no reconocida para archivo: {archivo}")
+                        continue
+                    es_nuevo = not os.path.isfile(ruta)
+                    with open(ruta, mode='a', newline='', encoding='utf-8') as f:
+                        writer = csv.DictWriter(f, fieldnames=resultados_dam.keys()) 
+                        if es_nuevo:
+                            writer.writeheader()
+                        writer.writerow(resultados_dam)
+                    logging.info(f"** FIN ** Procesamiento DAM terminado para {archivo}")
+                    
                     
                 '''
-                elif tipo == "DAM":
-                    procesar_dam(texto_relevante)
                 elif tipo == "BPAP":
                     procesar_bpap(texto_relevante)
                 elif tipo == "ACTIGRAFIA":
